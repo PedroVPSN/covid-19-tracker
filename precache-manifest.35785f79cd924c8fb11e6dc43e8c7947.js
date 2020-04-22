b02b310e9a46b928300ac8175afd9e2f936eada1
Pedro Silva <pedrosilvavpsn@gmail.com>
2020-04-22 14:03:27 +0100

    Updates

---
+++ precache-manifest.35785f79cd924c8fb11e6dc43e8c7947.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81a62d7c879de946921a1022f95e422",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "607b6c22be77e603f7f5",
+    "url": "/covid-19-tracker/static/css/main.98497953.chunk.css"
+  },
+  {
+    "revision": "e93fb24505373b687d1a",
+    "url": "/covid-19-tracker/static/js/2.553171c6.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid-19-tracker/static/js/2.553171c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607b6c22be77e603f7f5",
+    "url": "/covid-19-tracker/static/js/main.b04f7f24.chunk.js"
+  },
+  {
+    "revision": "3d1c3a839b5135420af2",
+    "url": "/covid-19-tracker/static/js/runtime-main.94eb6b52.js"
+  },
+  {
+    "revision": "b9c657be1fa683f27a266c981fb8c7e9",
+    "url": "/covid-19-tracker/static/media/covid19image.b9c657be.jpeg"
+  },
+  {
+    "revision": "1227a9cc588147d726bcf11039515962",
+    "url": "/covid-19-tracker/static/media/protect.1227a9cc.png"
+  },
+  {
+    "revision": "1cf61f1296c3ffc4c7b4349068b642de",
+    "url": "/covid-19-tracker/static/media/save.1cf61f12.png"
+  },
+  {
+    "revision": "bc849f94f25f4cfd78d2441aeb5078ec",
+    "url": "/covid-19-tracker/static/media/stay.bc849f94.png"
+  }
+]);
